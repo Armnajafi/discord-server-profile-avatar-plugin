e8653b893f131e6443a2436f872418cffbed8eeb
AmirMohammad Najafi <73462542+Armnajafi@users.noreply.github.com>
2022-09-06 22:49:26 +0430

    Update ServerProfileAvatar.plugin.js

--- ServerProfileAvatar.plugin.js
+++ ServerProfileAvatar.plugin.js
@@ -1,4 +1,4 @@
- /**
+/**
  * @name ServerProfileAvatar
  * @author Armnajafi
  * @description  A plugin to download the profile picture inside the profile server
@@ -11,32 +11,7 @@
  */
 
 module.exports = class ServerProfileAvatar {
-	
-	ServerAvatar(){
-			setTimeout(() => {
-				if(document.getElementsByClassName("layer-2aCOJ3")[0]){
-					if(document.getElementsByClassName("miniAvatar-dnzs_w")[0]){
-						let serverAvatar = document.getElementsByClassName("avatar-2Vndt_")[0].children[0].children[0].children[0].children[0].src;
-					
-						if(document.getElementsByClassName("headerTagWithNickname-1U8zZH")[0]){
-							if(document.getElementsByClassName("server-avatar-downloadLink").length > 0) return;
-							let HeaderTag = document.getElementsByClassName("headerTagWithNickname-1U8zZH")[0]
-							
-							let DownloadLink = document.createElement("a");
-							DownloadLink.innerHTML = "Server Avatar";
-							DownloadLink.style="color: #3365ab;float: right;font-weight: 900;";
-							DownloadLink.href = serverAvatar;
-							DownloadLink.className = "server-avatar-downloadLink";
-							DownloadLink.target = "_blank"
-
-							HeaderTag.append(DownloadLink);
-						}
-					}
-				}
-			} , 900)
-   }
-	
-   constructor(meta) {
+    constructor(meta) {
 	    if (window.ZLibrary) {
 				ZLibrary.PluginUpdater.checkForUpdate(
 				  this.getName(),
@@ -46,14 +21,23 @@ module.exports = class ServerProfileAvatar {
 	    }
 			BdApi.showToast("ServerProfileAvatar is Ready to start", {type: "success"});
 	}
-	
+	stop() {}
     start() {
-     	document.addEventListener("click" , this.ServerAvatar)
+		this.stop = document.removeEventListener.bind(document, "click", SPAFunction, true);
+		function SPAFunction({ target }){
+			if (target.classList.contains("avatar-2Vndt_") && document.getElementsByClassName("miniAvatar-dnzs_w")[0]) {
+				BdApi.showConfirmationModal("ServerProfileAvatar", `Do you want to see picture?`, {
+					confirmText: "Download Now",
+					cancelText: "Cancel",
+					onCancel: _ => {},
+					onConfirm: _ => {
+						window.open(target.querySelector("img").src.replace(/(?:\?size=\d{3,4})?$/, "?size=4096"), "_blank");
+					}
+				});
+		    }
+		}
+
+     	document.addEventListener("click" , SPAFunction, true)
 		BdApi.showToast("ServerProfileAvatar is Started", {type: "success"});
     } 
-	
-    stop() {
-        document.removeEventListener("click" , this.ServerAvatar)
-		BdApi.showToast("ServerProfileAvatar is Stoped");
-    }
 }
